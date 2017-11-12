531ea7d2778bfac9f715eaa886b535cfb6163e91
banuays <banuays@gmail.com>
2017-11-12 16:16:21 +0100

    Add files via upload

---
+++ readTypeE.js
@@ -0,0 +1,36 @@
+function readType()
+{
+    var xmlhttp = new XMLHttpRequest();
+    var returnval = "";
+    xmlhttp.onreadystatechange=function(){  
+        if(xmlhttp.readyState==4 && xmlhttp.status==200){
+            returnval=this.responseText;
+            return returnval;
+        }else{
+            return "filenotread";
+        }
+    }
+
+    //var path="http://prime.cs.vu.nl/logs/readType.php";
+    xmlhttp.open("GET", "./logs/readTypeE.php", false);
+    xmlhttp.send();
+    return xmlhttp.onreadystatechange();
+    //console.log(animationTypeString);
+}
+function writeCompletedExpType(expTypeCompleteText)
+{
+	var filename = "./experimentTypeE.txt";
+    var fd = new FormData();
+	fd.append('data', expTypeCompleteText);
+    fd.append('logfile', filename);
+	//fd.append('tstamp', timestamp);
+	
+	var xhrForm = new XMLHttpRequest();
+
+	//var path = "http://www.aysolmaz.com/prime/logs/log.php";
+    var path = "http://prime.cs.vu.nl/logs/writeFinishedExpType.php";
+	xhrForm.open("POST", path, false);
+
+	xhrForm.send(fd); 
+	//alert('done');
+}
